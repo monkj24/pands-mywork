5d4550462392403b35c269bbf186cee5ed23caac
monkj24 <j.mnich76@gmail.com>
2025-02-09 11:24:42 +0000

    nameAndage

---
+++ Week1/helloWorld.py
@@ -0,0 +1,5 @@
+# hello World
+# This program just prints out hello World
+# Author Joanna Mnich
+
+print("Hello World")
---
+++ Week2/nameAndage.py
@@ -0,0 +1,11 @@
+# nameAndage
+# reads in a name and age and outputs
+# author Joanna Mnich
+
+'''
+print ('Hello Andrew\t, ' + 'your age is 21.')
+'''
+
+print(21 - 4)
+
+print(2==3)
