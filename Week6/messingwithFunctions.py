b3b5923902c2720c036c7a0ebd90b37aaf8e5e0d
monkj24 <j.mnich76@gmail.com>
2025-03-10 10:55:59 +0000

    changes

--- Week6/messingwithFunctions.py
+++ Week6/messingwithFunctions.py
@@ -2,9 +2,9 @@
 #to demonstrate the defining and using functions
 
 x,y,z = (1 , 2 , 3)
-print(x,y,z, sep="", end="")
-print(f"(x)-(y)-(z)")
-print("{} {}--{}", format(x , y, z))
+#print(x,y,z, sep="", end="")
+#print(f"{x}-{y}-{z}")
+#print("{} {}--{}", format(x , y, z))
 
 def cube(number):
         print(number)
@@ -12,9 +12,9 @@ def cube(number):
 
 cube(23)
 ans = cube(23)
-print(f"we got answer" {ans})
+print(f"we got answer {ans}")
     
 
-num =45
-print(f"and {num} is {cub(num)}")
+num = 45
+print(f"and {num} is {cube(num)}")
 
