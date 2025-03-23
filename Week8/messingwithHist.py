2e22c8a5e3f56e3a5a68bb16bd489610328e1c76
monkj24 <j.mnich76@gmail.com>
2025-03-23 19:47:54 +0000

    week8

---
+++ Week8/messingwithHist.py
@@ -0,0 +1,19 @@
+# messing with histogram
+
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+np.random.seed(1)   #for see the same random data, 
+#without that code  histogram when runing again and again will be different
+normData = np.random.normal(size=10000)
+
+plt.hist(normData)
+plt.show()'
+'''
+
+fruit = np.array(['Apples', 'Orange', 'Bannana'])
+numbers = np.array([23,77,500])
+
+plt.pie(numbers, labels = fruit)
+plt.legend()
+plt.show()
---
+++ Week8/messingwithNumpy.py
@@ -0,0 +1,18 @@
+# Messing with numpy
+ # Author : Joanna Mnich
+
+import numpy as np
+
+list_of_numbers=[1,2,3,"asdf"]
+print ("list",list_of_numbers)
+
+numbers = np.array([1,2,3,4])
+numbers= numbers+3
+numbers = numbers*[1,2,3,4]
+print("array", numbers)
+
+rando =np.random.randint(100,200,30)
+print(rando)
+normalrando = np.random.normal(loc=50, scale=20, size=100)
+print(normalrando)
+                
