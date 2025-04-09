1f379930d8639343c406172553264d423cdb49a5
monkj24 <j.mnich76@gmail.com>
2025-04-09 11:15:16 +0100

    week11

---
+++ Week11/health.py
@@ -0,0 +1,16 @@
+# use person module
+
+#Author joanna Mnixh
+
+import datetime as dt
+from personmodule import *
+
+person = {
+       'firstname': 'andrew',
+       'lastname' : 'beatty',
+       'dob' : dt.date(2010,1,1),
+       'height' : 180,
+       'width' : 100
+}
+displayperson(person)
+gethealthdata(person)
---
+++ Week11/personmodule.py
@@ -0,0 +1,22 @@
+# demonstration of a module
+
+#Author Joanna Mnich
+
+import datetime as dt
+def gethealthdata(person):
+    print("get health data:", person['firstname'])
+
+
+def displayperson(person):
+    print(person)
+
+if __name__ == '__main__':
+    person = {
+       'firstname': 'andrew',
+       'lastname' : 'beatty',
+       'dob' : dt.date(2010,1,1),
+       'height' : 180,
+       'width' : 100
+}
+displayperson(person)
+gethealthdata(person)
