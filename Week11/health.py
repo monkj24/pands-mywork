24824b7b7e812abbc7d925c7bf37a804078ea712
monkj24 <j.mnich76@gmail.com>
2025-04-09 11:20:28 +0100

    corection

--- Week11/health.py
+++ Week11/health.py
@@ -2,15 +2,17 @@
 
 #Author joanna Mnixh
 
+
 import datetime as dt
 from personmodule import *
 
 person = {
-       'firstname': 'andrew',
-       'lastname' : 'beatty',
-       'dob' : dt.date(2010,1,1),
-       'height' : 180,
-       'width' : 100
+    'firstname': 'andrew',
+    'lastname': 'beatty',
+    'dob': dt.date(2010, 1, 1),
+    'height': 180,
+    'width': 100
 }
+
 displayperson(person)
 gethealthdata(person)
--- Week11/personmodule.py
+++ Week11/personmodule.py
@@ -9,7 +9,7 @@ def gethealthdata(person):
 
 def displayperson(person):
     print(person)
-
+'''
 if __name__ == '__main__':
     person = {
        'firstname': 'andrew',
@@ -20,3 +20,4 @@ if __name__ == '__main__':
 }
 displayperson(person)
 gethealthdata(person)
+'''
