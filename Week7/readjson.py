378af4fcc0417041e18b4a6cb6fad15a16d169e6
monkj24 <j.mnich76@gmail.com>
2025-05-10 07:43:13 +0100

    week7

--- Week7/readjson.py
+++ Week7/readjson.py
@@ -3,7 +3,8 @@ FILENAME ="testdict.json"
  
 def readDict(): 
     # this will throw an error if the file does 
-    not exist 
+    
+    not exit
     # it should readly just return an empty dict  
     # we can do this later 
     with open(FILENAME) as f: 
